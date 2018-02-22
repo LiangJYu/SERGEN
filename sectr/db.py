76c7066ed5bf66bbbe3644f09ee317fac6e67db2
Liang Yu <liangjyu@gmail.com>
2018-02-22 14:57:44 -0800

    working transaction_db class
    added test db file name and SQL command

--- sectr/db.py
+++ sectr/db.py
@@ -3,16 +3,25 @@ import sqlite3
 class transaction_db:
 
     def __init__(self):
-        self.db_file = ''
+        self.db_file = '../data/transactions.db'
         self.conn = ''
 
     def create_connection(self):
         try:
-            self.conn = sqlite3.connect()
+            self.conn = sqlite3.connect(self.db_file)
         except Error as e:
             print(e)
     
-    def create_table(self, sql):
+    def create_table(self):
+        sql = """ CREATE TABLE IF NOT EXISTS transactions(
+                        date text PRIMARY KEY,
+                        amount float,
+                        description text NOT NULL,
+                        lender text NOT NULL,
+                        main_category text NOT NULL,
+                        sub_category text NOT NULL,
+                        exta_src_info text NOT NULL,
+                        notes text NOT NULL); """
         try:
             c = self.conn.cursor()
             c.execute(sql)
